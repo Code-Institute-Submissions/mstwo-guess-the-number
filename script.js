2790f634d6c1bf2419d61c02af3eaad8021511cf
kafamem <kafamem@gmail.com>
2021-05-11 20:24:23 +0000

    guess not equal to secretNumber scenario

--- script.js
+++ script.js
@@ -9,17 +9,26 @@ document.querySelector('.check').addEventListener(
         const guess = Number(document.querySelector('.guess').value);
         console.log(guess, typeof guess);
 
+        // Check button click in case of no input
         if (!guess) {
-            document.querySelector('.message').textContent = '🛑 No number!';
-            // Check button click in case of no input
-
+            document.querySelector('.message').textContent = '🛑 No number!';   
+        // If guess matches with the secretNumber
         } else if (guess === secretNumber) {
             document.querySelector('.message').textContent = '🎉 Wow! Exact number';
             document.querySelector('.number').textContent = secretNumber;
+            // Change in style upon winning both bg & number width
             document.querySelector('body').style.backgroundColor = '#1F2833';
             document.querySelector('.number').style.width = '30rem';
+        // The number is higher or lower than the correct guess
+        } else if (guess !== secretNumber) {
+            if (score > 1) {
+                document.querySelector('.message').textContent = guess > secretNumber ? 'This number is high, try again!': 'This number is low';
+                score--;
+                document.querySelector('.score').textContent = score;
+            } else {
+                document.querySelector('message').textContent = '😥 You lost the game!';
+                document.querySelector('.score').textContent = 0;
+            }
         }
-        // If guess matches with the secretNumber
-
     }
 )
