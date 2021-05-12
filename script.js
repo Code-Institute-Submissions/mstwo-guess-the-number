7e0a5a7897bbf35eaf4e5679486723f27184902e
kafamem <kafamem@gmail.com>
2021-05-12 09:14:27 +0000

    Feat: adding 'Again' eventListner & handler fnct

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// Variable for random numbers between (1 to/and 20) without decimals
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 
@@ -16,13 +17,15 @@ document.querySelector('.check').addEventListener(
         } else if (guess === secretNumber) {
             document.querySelector('.message').textContent = '🎉 Wow! Exact number';
             document.querySelector('.number').textContent = secretNumber;
-            // Change in style upon winning both bg & number width
+            // Change in style upon winning: both bg & number width
             document.querySelector('body').style.backgroundColor = '#1F2833';
             document.querySelector('.number').style.width = '30rem';
         // The number is higher or lower than the correct guess
         } else if (guess !== secretNumber) {
             if (score > 1) {
+                // ternary operator for both high and low number missmatching scenarios
                 document.querySelector('.message').textContent = guess > secretNumber ? 'This number is high!': 'This number is low';
+                // Score decrement upon guess matching trials
                 score--;
                 document.querySelector('.score').textContent = score;
             } else {
@@ -31,4 +34,13 @@ document.querySelector('.check').addEventListener(
             }
         }
     }
-)
+);
+// 'Again' button click
+document.querySelector('.again').addEventListener(
+    'click', function() {
+        score = 20;
+        // Value reassigned from the let secretNumber variable
+        secretNumber = Math.trunc(Math.random() * 20) + 1;
+                
+    }
+);
