5cfd8a4533aec37c1b8e6f81629e91d9036a2829
kafamem <kafamem@gmail.com>
2021-05-19 19:50:06 +0000

    functions commented

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -4,27 +4,29 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let attempt = 20;
 let topscore = 0;
 
+// Check eventListner upon clicking
 document.querySelector('.check').addEventListener(
     'click', function() {
         const guess = Number(document.querySelector('.guess').value);
-        console.log(guess, typeof guess);
-
+        console.log(guess);
+        // Scenario where check button hit without input
         if (!guess) {
             document.querySelector('.message').textContent = '🛑 No number! Type in your number.';   
-
+        // Scenario where guess matches with the secretNumber
         } else if (guess === secretNumber) {
             document.querySelector('.message').textContent = '🎉 Wow! Exact number!';
             document.querySelector('.number').textContent = secretNumber;
             document.querySelector('.number').style.width = '30rem';
-
+            // Implementing topscore
             if (attempt > topscore) {
                 topscore = attempt;
                 document.querySelector('.topscore').textContent = topscore;
             }
+        // scenario where the guess is higher or lower than the secreNumber
         } else if (guess !== secretNumber) {
             if (attempt > 1) {
                 document.querySelector('.message').textContent = guess > secretNumber ? '📈 This number is high!': '📉 This number is low!';
-
+                // Upon failing to guess correctly, number of attempts reduces to zero
                 attempt--;
                 document.querySelector('.attempt').textContent = attempt;
             } else {
@@ -34,7 +36,7 @@ document.querySelector('.check').addEventListener(
         }
     }
 );
-
+// Play again upon clicking 'Again' button
 document.querySelector('.again').addEventListener(
     'click', function() {
         attempt = 20;
